862bf4d3f8dc04b67bca8b39119f753f96089e74
Kharag <kharag94@gmail.com>
2024-11-21 11:14:18 -0500

    class account and def init

--- banking.py
+++ banking.py
@@ -1,8 +1,25 @@
 # the start
 class Account:
-    def accountNumber(self,number):
-        self.number = number
+    def getAccountNumber(self):
+        self.accNumber
 
-    def accountHolderName(self,name):
-        self.name = name
-        
+    def getAccountHolderName(self):
+        self.accName
+
+    def getRateOfInterest(self):
+        self.rateInterest
+
+    def GetCurrentBalance(self):
+        self.theBalance
+
+    def _init_(self,account_number,account_name,rate_interest,current_balance):
+        self.accNumber = account_number
+        self.accName = account_name
+        self.rateInterest = rate_interest
+        self.theBalance = current_balance
+
+    def setRateofInterest(self,rate):
+        self.rateInterest = rate
+
+    def setAccountHolderName(self,name):
+        self.accName = name
